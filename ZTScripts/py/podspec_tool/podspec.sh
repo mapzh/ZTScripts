fdcc3e8c95d8af03a98766195afb58432913dad2
mapengzhen <mapengzhen@rd.tuan800.com>
2016-09-19 21:17:22 +0800

    auto commit

--- ZTScripts/py/podspec_tool/podspec.sh
+++ ZTScripts/py/podspec_tool/podspec.sh
@@ -13,38 +13,51 @@ find_tag_version(){
     version=0
     file=$1
     if [[ -f ${file} ]]; then
-        version=`cat ${file} |  grep '^  *s.version *=' | awk -F"[']" '{print $2}'`
+        version=`cat ${file} |  grep '^ *s.version *=' | awk -F"[']" '{print $2}'`
     fi
     echo $version
 }
 
-change_revision_version(){
-    file=$1
-    version=`find_tag_version ${file}`
-    new_version=`echo ${version} | awk -F"[.]" '
-        BEGIN{
 
+change_version(){
+    file=$1
+    tag_location=$2
+    version=`find_tag_version ${file}`
+    new_version=`echo ${version} | awk -F"[.]" -v location=$tag_location '
+        BEGIN{
 
         }
         {
-            printf("%s.%s.%d",$1,$2,$3+1);
         }
         END{
-
+            if (location==1){
+                printf("%d.%s.%s",$1+1,$2,$3);
+            }else if(location==2){
+                printf("%s.%d.%s",$1,$2+1,$3);
+            }else if(location==3){
+                printf("%s.%s.%d",$1,$2,$3+1);
+            }
         }
     '`
-    cat $file | sed -i "s/"${version}"/"${new_version}"/g" *
+    sed -i 'tmp' 's/'${version}'/'${new_version}'/g' $file
+    rm -r "${file}tmp"
+}
+
+change_revision_version(){
+    change_version $1 3
 }
 
 change_minor_version(){
 
-    pass
+    change_version $1 2
 }
 
 change_major_version(){
 
-pass
+    change_version $1 1
 }
+
+
 
 while getopts :f:r:m:M:: ARGS
     do
